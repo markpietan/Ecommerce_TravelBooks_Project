25cc56ea5d8e38a207b2778659eaeb0797c5f559
Your Name <your_email@real_email.edu>
2020-07-12 16:04:58 -0400

    worked on Material UI Library and Login Function

--- src/components/Links/Login.js
+++ src/components/Links/Login.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import Button from "@material-ui/core/Button";
 
 function Login() {
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  
+  function onEmailChange(e){
+   setEmail(e.target.value)
+  }
+
+  function onPasswordChange(e){
+    setPassword(e.target.value)
+   }
+
   return (
     <div className="loginPage">
       <h1 className="loginTitle">Login Page</h1>
@@ -12,12 +24,16 @@ function Login() {
           type="text"
           placeholder="Enter email..."
           className="emailInput"
+          value={email}
+          onChange={onEmailChange}
         ></input>
         <h2 className="passwordText">Password:</h2>
         <input
-          type="text"
+          type="password"
           placeholder="Enter Password..."
           className="passwordInput"
+          value={password}
+          onChange={onPasswordChange}
         ></input>
         <div className="loginButton">
           <Button variant="contained">Login</Button>
--- src/components/Links/SignUp.js
+++ src/components/Links/SignUp.js
@@ -1,39 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
-import Button from "@material-ui/core/Button";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  FormHelperText,
+  Input,
+  Container,
+  TextField,
+} from "@material-ui/core";
 
-function SignUp() {
+// function SignUp() {
+//   function onEmailChange(e) {
+//     setEmail(e.target.value);
+//   }
+
+//   function onPasswordChange(e) {
+//     setPassword(e.target.value);
+//   }
+
+//   function onConfirmPasswordChange(e) {
+//     setConfirmPassword(e.target.value);
+//   }
+
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
   return (
-    <div className="signUpPage">
-      <h1 className="signUpTitle">Sign Up Page</h1>
-      <div className="signUpSection">
-        <h2 className="emailText">Email:</h2>
-        <input
-          type="text"
-          placeholder="Enter email..."
-          className="emailInput"
-        ></input>
-        <h2 className="passwordText">Password:</h2>
-        <input
-          type="text"
-          placeholder="Enter Password..."
+    <Container>
+      <form autoComplete>
+        <TextField
+          id="standard-full-width"
+          label="Email"
+          style={{ margin: 8 }}
+          placeholder="Enter email"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          id="standard-full-width"
+          label="Email"
+          style={{ margin: 8 }}
+          placeholder="Enter email"
+          size="medium"
+          variant="outlined"
+          margin="normal"
           className="passwordInput"
-        ></input>
-        <h2 className="passwordText">Confirm Password:</h2>
-        <input
-          type="text"
-          placeholder="Confirm Password..."
+        />
+        <TextField
+          id="standard-full-width"
+          label="Email"
+          style={{ margin: 8 }}
+          placeholder="Enter email"
+          size="medium"
+          margin="normal"
           className="passwordInput"
-        ></input>
-        <div className="signUpButton">
-          <Button variant="contained">Sign Up</Button>
-        </div>
-        <div className="googleSignUp">
-          <Button variant="contained">Sign Up With Google</Button>
-        </div>
-      </div>
-    </div>
+        />
+      </form>
+    </Container>
   );
-}
+  // return (
+  //   <div className="signUpPage">
+  //     <h1 className="signUpTitle">Sign Up Page</h1>
+  //     <div className="signUpSection">
+  //       <h2 className="emailText">Email:</h2>
+  //       <input
+  //         type="text"
+  //         placeholder="Enter email..."
+  //         className="emailInput"
+  //         value={email}
+  //         onChange={onEmailChange}
+  //       ></input>
+  //       <h2 className="passwordText">Password:</h2>
+  //       <input
+  //         type="password"
+  //         placeholder="Enter Password..."
+  //         className="passwordInput"
+  //         value={password}
+  //         onChange={onPasswordChange}
+  //       ></input>
+  //       <h2 className="passwordText">Confirm Password:</h2>
+  //       <input
+  //         type="password"
+  //         placeholder="Confirm Password..."
+  //         className="passwordInput"
+  //         value={confirmPassword}
+  //         onChange={onConfirmPasswordChange}
+  //       ></input>
+  //       <div className="signUpButton">
+  //         <Button variant="contained">Sign Up</Button>
+  //       </div>
+  //       <div className="googleSignUp">
+  //         <Button variant="contained">Sign Up With Google</Button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+// }
 
-export default SignUp;
+// export default SignUp;
