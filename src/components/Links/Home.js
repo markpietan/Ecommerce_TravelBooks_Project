ecba8ab396a561629a8c6f90d86b231a8dd291e2
Ian A <n01384067@unf.edu>
2020-07-12 15:47:50 -0400

    signup

--- src/components/Links/Home.js
+++ src/components/Links/Home.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
-import Button from "@material-ui/core/Button";
+import Item from "./Item.js";
 
 function Home() {
+  const [items, setItems] = useState([1, 2, 3, 4, 5]);
+
   return (
     <div className="homePage">
       <h1 className="title">Home Page</h1>
       <h3 className="testTitle">Products down below</h3>
-
+      {items.map((item) => {
+        return <Item item={item}></Item>;
+      })}
       <div className="productSection"></div>
     </div>
   );
--- src/components/Links/Item.js
+++ src/components/Links/Item.js
@@ -1,16 +1,30 @@
-import React from "react";
-export default function Item() {
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+
+export default function Item({ item }) {
+  const [number, setNumber] = useState(1);
+  const incrementClickCount = () => {
+    setNumber(number + 1);
+  };
+  const deccrementClickCount = () => {
+    setNumber(number - 1);
+  };
+
   return (
     <div className="testProduct">
       <div className="importantText">
-        <h4>Test Product 1</h4>
+        <h4>item</h4>
         <h4>img here...</h4>
         <h4>Price</h4>
       </div>
       <div className="add_remove">
-        <Button className="minus">-</Button>
-        <h4 className="quantity"> 1 </h4>
-        <Button className="plus">+</Button>
+        <Button className="minus" onClick={deccrementClickCount}>
+          -
+        </Button>
+        <h4 className="quantity"> {number} </h4>
+        <Button className="plus" onClick={incrementClickCount}>
+          +
+        </Button>
       </div>
     </div>
   );
