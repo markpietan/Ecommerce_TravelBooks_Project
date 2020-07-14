7685757e3c83c03d8ac06603c3c9e7a7dc1017fa
Andres Soto-Martinez <andressoto-martinez@AndresSinezsMBP.attlocal.net>
2020-07-14 19:19:23 -0400

    stripe button

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 // This is the Web Server
-const express = require('express');
+const express = require("express");
 const server = express();
+const stripe = require("stripe")(
+  "sk_test_518heLIHvs9PrrMbVIDjZ7IHuy45NCXN3NWU4ShglQg7ggDKHfPVPOLch8uquGCJJb3yNAddJFNexTpaSscYRK5SU00TlT3CV1u"
+);
+const uuid = require("uuid/v4");
 
 // create logs for everything
-const morgan = require('morgan');
-server.use(morgan('dev'));
+const morgan = require("morgan");
+server.use(morgan("dev"));
 
 // handle application/json requests
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 server.use(bodyParser.json());
 
 // here's our static files
-const path = require('path');
-server.use(express.static(path.join(__dirname, 'build')));
+const path = require("path");
+server.use(express.static(path.join(__dirname, "build")));
 
 // here's our API
-server.use('/api', require('./routes'));
+server.use("/api", require("./routes"));
+server.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  const idempontencyKey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: customer.id,
+          receipt_email: token.email,
+          description: `Purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempontencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((error) => console.log(error));
+});
 
 // by default serve up the react app if we don't recognize the route
 // server.use((req, res, next) => {
@@ -23,16 +56,16 @@ server.use('/api', require('./routes'));
 // });
 
 // bring in the DB connection
-const { client } = require('./db');
+const { client } = require("./db");
 
 // connect to the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, async () => {
-  console.log(`Server is running on ${ PORT }!`);
+  console.log(`Server is running on ${PORT}!`);
 
   try {
     await client.connect();
-    console.log('Database is open for business!');
+    console.log("Database is open for business!");
   } catch (error) {
     console.error("Database is closed for repairs!\n", error);
   }
--- src/components/Links/Home.js
+++ src/components/Links/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Button from "@material-ui/core/Button";
 import Item from "./Item.js";
