5bb70bfa11cfa577d8247513fbe3129bab774801
Your Name <your_email@real_email.edu>
2020-07-21 20:02:56 -0400

    uncommeneted Stripe

--- src/components/Links/Cart.js
+++ src/components/Links/Cart.js
@@ -97,7 +97,7 @@ function Cart({ cart, removeFromCart, addToCart }) {
         );
       })}
       <br></br>
-      {/* <StripeCheckout
+      <StripeCheckout
         stripeKey="pk_test_518heLIHvs9PrrMbVm46vQX5P5oQ17M7GTeJlBElSccp6l6f4oGDhH7Mh6MfCZvjMDdFbxztQNoxrU04s8r3zqpN400xN34EUej"
         token={makePayment}
         name="Travel Books"
@@ -105,37 +105,13 @@ function Cart({ cart, removeFromCart, addToCart }) {
         shippingAddress
         billingAddress
         
-      > */}
+      >
         <Button className="paymentButton" variant="contained">
           Pay With Card
         </Button>
-      {/* </StripeCheckout> */}
+      </StripeCheckout>
     </Container>
 
-    // <div className="cartPage">
-    //   <h1 className="cartTitle">Cart Page</h1>
-    //   <div className="actualCart">
-    //     <h3 className="thisIsCart">This is your cart:</h3>
-    //     <h3 className="emptyCart">
-    //       Your cart is emtpy (only display if cart is emtpy)
-    //     </h3>
-    //     <h3 className="total">Total: (display total here)</h3>
-    //   </div>
-    //   <div className="paymentOption">
-    //     <StripeCheckout
-    //       stripeKey="pk_test_518heLIHvs9PrrMbVm46vQX5P5oQ17M7GTeJlBElSccp6l6f4oGDhH7Mh6MfCZvjMDdFbxztQNoxrU04s8r3zqpN400xN34EUej"
-    //       token={makePayment}
-    //       name="Travel Books"
-    //       amount={product.price * 100}
-    //       shippingAddress
-    //       billingAddress
-    //     >
-    //       <Button className="paymentButton" variant="contained">
-    //         Pay With Card
-    //       </Button>
-    //     </StripeCheckout>
-    //   </div>
-    // </div>
   );
 }
 
